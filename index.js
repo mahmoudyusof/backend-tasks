241369baaf7ac7a6839b4edcafc51e066d605000
Mahmoud Youssef <mahmoud.yusof27@gmail.com>
2019-10-26 19:48:52 +0200

    integrated mongodb, installed and used mongoose

--- index.js
+++ index.js
@@ -2,37 +2,52 @@
 
 // importing modules
 const express = require("express");
+const mongoose = require("mongoose");
 const Joi = require("joi");
 
 // creating app and configuring middlewares
 const app = express();
 app.use(express.json());
 
-// database data equivalent
-var students = [
-    {
-        id: 0,
-        name: "Mahmoud Youssef",
-        courses: ["Physics 2", "Math 3"]
-    },
-    {
-        id: 1,
-        name: "Mohammed Hassan",
-        courses: ["Software engineering", "CO"]
-    },
-    {
-        id: 2,
-        name: "Hossam Mahmoud",
-        courses: ["Distribution", "Power Electronics"]
-    },
-];
+mongoose.connect("mongodb://localhost:27017/playground", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true})
+    .then(() => console.log("Connected to mongo DataBase"))
+    .catch(err => console.log(err));
+
+
+
+const studentsSchema = new mongoose.Schema({
+    name: String,
+    courses: [ String ]
+});
+
+const Student = mongoose.model('Student', studentsSchema);
+
+// // database data equivalent
+// var students = [
+//     {
+//         id: 0,
+//         name: "Mahmoud Youssef",
+//         courses: ["Physics 2", "Math 3"]
+//     },
+//     {
+//         id: 1,
+//         name: "Mohammed Hassan",
+//         courses: ["Software engineering", "CO"]
+//     },
+//     {
+//         id: 2,
+//         name: "Hossam Mahmoud",
+//         courses: ["Distribution", "Power Electronics"]
+//     },
+// ];
 
 /**
  * this function returns all student data
  * @params none
  * @body none
  */
-app.get('/api/students', (req, res) => {
+app.get('/api/students', async (req, res) => {
+    const students = await Student.find();
     res.send(students);
 });
 
@@ -41,10 +56,18 @@ app.get('/api/students', (req, res) => {
  * @params id: number
  * @body none
  */
-app.get("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id === parseInt(req.params.id));
-    if (!student) return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
-    res.send(student);
+app.get("/api/students/:id", async (req, res) => {
+    try{
+        const student = await Student.findById(req.params.id);
+        if(student === null){
+            return res.status(404).send("Student with ID = \"" + req.params.id + "\" does not exist!");
+        }
+        res.send(student);
+    }catch(err){
+        return res.status(400).send("Bad Request");
+    }
+    // if (!student) return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
+    
 });
 
 /**
@@ -52,7 +75,7 @@ app.get("/api/students/:id", (req, res) => {
  * @params none
  * @body student data
  */
-app.post("/api/students", (req, res) => {
+app.post("/api/students", async (req, res) => {
     const schema = {
         name: Joi.string().min(3).required(),
         courses: Joi.array().min(2).required()
@@ -62,8 +85,8 @@ app.post("/api/students", (req, res) => {
     if (result.error)
         return res.status(400).send(result.error.details[0].message);
 
-    var obj = {id: students.length, ...req.body};
-    students.push(obj);
+    let student = new Student(req.body);
+    await student.save();
     res.send("Student added successfully!");
 });
 
@@ -72,9 +95,9 @@ app.post("/api/students", (req, res) => {
  * @params id
  * @body new student data
  */
-app.put("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id === parseInt(req.params.id));
-    if (!student) return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
+app.put("/api/students/:id", async (req, res) => {
+    // const student = students.find(s => s.id === parseInt(req.params.id));
+    // if (!student) return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
 
     const schema = {
         name: Joi.string().required().min(3),
@@ -84,10 +107,17 @@ app.put("/api/students/:id", (req, res) => {
     const result = Joi.validate(req.body, schema);
     if (result.error)
         return res.status(400).send(result.error.details[0].message);
-
-    var idx = students.indexOf(student);
-    students[idx] = {id: students[idx].id, ...req.body};
-    res.send("Student updated successfully!");
+    try{
+        const student = await Student.findByIdAndUpdate(req.params.id, req.body);
+        if(student === null){
+            return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
+        }
+        res.send("Student updated successfully!");
+    }catch(error){
+        return res.status(400).send("Bad request");
+    }
+    // var idx = students.indexOf(student);
+    // students[idx] = {id: students[idx].id, ...req.body};
 });
 
 /**
@@ -95,13 +125,17 @@ app.put("/api/students/:id", (req, res) => {
  * @params id
  * @body none
  */
-app.delete("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id === parseInt(req.params.id));
-    if (!student) return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
-
-    const idx = students.indexOf(student);
-    students.splice(idx, 1);
-    res.send("Student deleted successfully!")
+app.delete("/api/students/:id", async (req, res) => {
+    try{
+        const student = await Student.findByIdAndDelete(req.params.id);
+        if(student === null){
+            return res.status(404).send("Student of ID = " + req.params.id + " does not exist!");
+        }else{
+            res.send("Student deleted successfully!")
+        }
+    }catch(err){
+        return res.status(400).send("Bad request!");
+    }
 });
 
 // now it's time to listen to some port
